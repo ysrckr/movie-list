af9f229dd8b731b195cfb1cce360889ed4b5ef76
ysrckr <dyt.yasarcakir@gmail.com>
2023-01-09 00:23:37 +0300

    handler is in usecallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Type } from './options/type';
 import { Country } from './options/country';
-
-
 import { Service } from './options/service';
-
 import {SelectChangeEvent} from '@mui/material';
 import useDebounce from './hooks/useDebounce';
 import { useGetMovieList } from './hooks/useGetMovieList';
@@ -20,16 +17,13 @@ export default function App() {
 
   const debouncedSearch = useDebounce(searchQuery, 1000);
 
-
-  
-
   const {
     data: movies,
     isLoading,
     refetch,
   } = useGetMovieList({type, country, service});
 
-  const handleChange = (e: SelectChangeEvent) => {
+  const handleChange = useCallback((e: SelectChangeEvent) => {
     switch (e.target.name) {
       case 'type':
         SetType(e.target.value as Type);
@@ -43,7 +37,7 @@ export default function App() {
       default:
         break;
     }
-  };
+  }, [type, country, service]);
 
   return (
     <div className='App m-5'>
